593a13dcf94aeb45cd066605aafc29e8c3c90fdc
karthi1048 <kesavankarthikeyan47@gamil.com>
2025-08-29 17:12:40 +0530

    Added sample tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
 
 export default function App() {
-  const columns = ["To Do", "In Progress", "Done"];
+  // Column names: Array of tasks
+  const board = {
+    "To DO": ["Task 1", "Task 2"],
+    "In Progress": ["Task 3"],
+    "Done": ["Task 4"],
+  }
 
   return (
     <>
@@ -9,11 +14,17 @@ export default function App() {
 
       {/* Board */}
       <div className='flex h-screen bg-gray-100 p-6 gap-6'>
-        {/* Iterate over columns & return div for each column */}
-        { columns.map((col) => (
+        {/* Get [key, value] & destructure as [col, tasks] & return div for each column*/}
+        { Object.entries(board).map(([col, tasks]) => (
           // Columns
           <div key={col} className='flex-1 bg-white rounded-2xl shadow-md p-4'>
-            <h2 className='text-xl font-bold mb-4'>{col}</h2>      
+            <h2 className='text-xl font-bold mb-4'>{col}</h2>
+            {/* Similar to columns, return div for each task */}
+            { tasks.map((task) => (
+              <div key={task} className='p-3 mb-2 bg-blue-500 text-white rounded-lg shadow cursor-pointer'>
+                {task}
+              </div>
+            ))}
           </div>
         ))}
       </div>
